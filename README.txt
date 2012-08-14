f7e02a33077093bb2904ea70ae52ecda17a886a8
Kevin Cantrell <klcant@gmail.com>
2012-08-13 22:23:17 -0400

    Added README.txt and bash script

---
+++ README.txt
@@ -0,0 +1,2 @@
+This is a quick and easy bash script used to copy a file (or files) from a Windows box to a Linux box. 
+The files are then emailed to others.
---
+++ move_files_win_to_linux_and_mail_results.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+# Mount windows share and use file for network credentials
+# /mnt/win/folder folder created for windows smb mounts. I setup a serarate folder for each share
+sudo mount -t smbfs //windows_server/share/folder /mnt/win/folder -o credentials=/etc/.smbpasswd
+
+# Check to see if file(s) exists. If is is there then run through the if statement
+if [ -e /mnt/win/folder/file*.xls ]
+then
+	# Move file(s) from mount to local directory
+	sudo mv /mnt/win/folder/file*.xls /home/user/arch/server/
+	
+	# Unmount the windows share
+	sudo umount /mnt/win/folder
+
+	# Email address email will be coming from
+	EMAIL="FirstName LastName <sender@domain.com>"
+	export EMAIL
+
+	# Use mutt CLI to send message
+	mutt -s "Subject" -a /home/user/arch/server/file*.xls -- recipient@domain.com < /home/user/arch/server/message.txt
+
+	# Create directory to archive file to (YYYY/MM/DD)
+	mkdir /home/user/arch/server/subfldr/$(date +%Y%m%d)
+
+	# Move file(s) to archive folder
+	sudo mv /home/user/arch/server/file*.xls /home/user/arch/server/subfldr/$(date +%Y%m%d)
+fi
