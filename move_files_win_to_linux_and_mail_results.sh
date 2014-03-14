7da6fbb9046c8a4acdec141a00bd88231c7f95c0
klcant <klcant@gmail.com>
2014-03-14 16:16:01 -0400

    update if statement

--- move_files_win_to_linux_and_mail_results.sh
+++ move_files_win_to_linux_and_mail_results.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 
+clear
 
 # Mount windows share and use file for network credentials
 # /mnt/win/folder folder created for windows smb mounts. I setup a serarate folder for each share
-sudo mount -t smbfs //windows_server/share/folder /mnt/win/folder -o credentials=/etc/.smbpasswd
+mount -t cifs -o credentials=/path/to/creds/secret.txt //windows_server/share/folder /mnt/win/folder
+
+sleep 15
+
+echo "mount complete"
 
 # Check to see if file(s) exists. If is is there then run through the if statement
-if [ -e /mnt/win/folder/file*.xls ]
-then
-	# Move file(s) from mount to local directory
-	sudo mv /mnt/win/folder/file*.xls /home/user/arch/server/
+if [ ! -f /mnt/win/folder/file*.xls ]
+	then
+		echo "file does not exist."
+	else
+		echo "entering else statement"
+		# Move file(s) from mount to local directory
+		sudo mv /mnt/win/folder/file*.xls /home/user/arch/server/
 
-	# Email address email will be coming from
-	EMAIL="FirstName LastName <sender@domain.com>"
-	export EMAIL
+		# Email address email will be coming from
+		EMAIL="FirstName LastName <sender@domain.com>"
+		export EMAIL
 
-	# Use mutt CLI to send message
-	mutt -s "Subject" -a /home/user/arch/server/file*.xls -- recipient@domain.com < /home/user/arch/server/message.txt
+		# Use mutt CLI to send message
+		mutt -s "Subject" -a /home/user/arch/server/file*.xls -- recipient@domain.com < /home/user/arch/server/message.txt
 
-	# Create directory to archive file to (YYYY/MM/DD)
-	mkdir /home/user/arch/server/subfldr/$(date +%Y%m%d)
+		# Create directory to archive file to (YYYY/MM/DD)
+		mkdir /home/user/arch/server/subfldr/$(date +%Y%m%d)
 
-	# Move file(s) to archive folder
-	sudo mv /home/user/arch/server/file*.xls /home/user/arch/server/subfldr/$(date +%Y%m%d)
+		# Move file(s) to archive folder
+		sudo mv /home/user/arch/server/file*.xls /home/user/arch/server/subfldr/$(date +%Y%m%d)
 fi
 
 # Unmount the windows share
